892144ee6bc393f0a0e8c225a72bb36621b7095b
Ben Roeder <ben@darkworks.net>
2026-01-02 01:22:03 +0000

    Remove __METAL_MACOS__/__METAL_IOS__ conditional
    These macros may not be defined during runtime Metal shader compilation
    via MTILibrarySourceRegistration. Since we're always compiling for Metal,
    the conditional is unnecessary and was preventing the metalpetal namespace
    from being defined.

--- Source/MTTransitionsSwiftPMSupport.mm
+++ Source/MTTransitionsSwiftPMSupport.mm
@@ -28,7 +28,9 @@ static const char *MTTransitionsBuiltinLibrarySource = R"mttrawstring(
 #ifndef MTTransitions_h
 #define MTTransitions_h
 
-#if __METAL_MACOS__ || __METAL_IOS__
+// Note: __METAL_MACOS__ and __METAL_IOS__ may not be defined during runtime
+// Metal compilation via MTILibrarySourceRegistration, so we unconditionally
+// include the Metal headers and definitions.
 
 #define PI 3.141592653589
 #define M_PI   3.14159265358979323846
@@ -242,8 +244,6 @@ namespace metalpetal {
       return fract(sin(dot(co.xy ,float2(12.9898,78.233))) * 43758.5453);
     }
 }
-
-#endif /* __METAL_MACOS__ || __METAL_IOS__ */
 
 #endif /* MTTransitions_h */
 
